c3c635073b0253d41d975e6f11fa261a3baea2a9
Falcao <henrique.m.falcao67@gmail.com>
2026-09-23 10:34:56 -0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,40 @@
+def registrarTentativas():
+  tentativas = []
+  i = 0
+  while i < 10:
+        valor = int(input(f"Insira o {i + 1}° valor: "))
+        if valor > 3 or valor < 0:
+            print("valor invalido fi")
+            continue
+        else:
+            tentativas.append(valor)
+            i += 1
+
+  return tentativas
+
+def calcularPontuacao(tentativas):
+    pontuacaoTotal = 0
+    for pontos in tentativas:
+        pontuacaoTotal += pontos
+        
+    return pontuacaoTotal
+
+def calcularAproveitamento(tentativas):
+    pontosConvertidos = 0
+    pontosNaoConvertidos = 0
+    for arremesso in tentativas:
+        if arremesso == 0:
+            pontosNaoConvertidos += 1
+        else:
+            arremessosConvertidos += 1
+            pontosConvertidos += arremesso
+            
+    print(f"Arremessos convertidos: {arremessosConvertidos}")
+    print(f"Pontos Não convertidos: {pontosNaoConvertidos}")
+    taxaDeAproveitamento = (arremessosConvertidos/10) * 100
+    
+    print(f"Taxa de aproveitamento: {taxaDeAproveitamento}%")
+    
+arremessos=registrarTentativas()
+
+print(calcularPontuacao(arremessos))
